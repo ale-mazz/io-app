e64c31cc48bf9a7e06949f42d52f6f4cdf3d5790
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2024-09-27 14:23:24 +0200

    feat: add newProfile reducer

---
+++ ts/features/newProfile/store/reducers/index.ts
@@ -0,0 +1,28 @@
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { getType } from "typesafe-actions";
+import { NetworkError } from "../../../../utils/errors";
+import { Action } from "../../../../store/actions/types";
+import NewProfile from "../../types";
+import { newProfileActions } from "../actions";
+
+export type NewProfileState = pot.Pot<NewProfile, NetworkError>;
+
+export const initialState: NewProfileState = pot.none;
+
+const reducer = (
+  state: NewProfileState = initialState,
+  action: Action
+): NewProfileState => {
+  switch (action.type) {
+    case getType(newProfileActions.request):
+      return pot.toLoading(state);
+    case getType(newProfileActions.success):
+      return pot.some(action.payload);
+    case getType(newProfileActions.failure):
+      return pot.toError(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
