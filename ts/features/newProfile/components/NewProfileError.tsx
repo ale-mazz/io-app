800277782841daa3805c1e988f36819c2121daef
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2024-10-04 14:42:00 +0200

    fix: onPress function mandatory in Error component

--- ts/features/newProfile/components/NewProfileError.tsx
+++ ts/features/newProfile/components/NewProfileError.tsx
@@ -5,14 +5,14 @@ import I18n from "../../../i18n";
 type NewProfileErrorProps = {
   title?: string;
   content?: string;
-  onPress?: () => void;
+  onPress: () => void;
 };
 
 const NewProfileError = ({ onPress, title, content }: NewProfileErrorProps) => (
   <Alert
     variant={"error"}
     action={I18n.t("global.buttons.retry")}
-    onPress={() => onPress && onPress()}
+    onPress={() => onPress()}
     title={title ?? I18n.t("profile.errors.load")}
     content={content ?? I18n.t("global.actions.retry")}
   />
